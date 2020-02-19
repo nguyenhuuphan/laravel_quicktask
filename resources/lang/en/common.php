1b3910ce93bfde11aae1831ef660cdbb2dc01584
nguyenhuuphan <phannguyen15496@gmail.com>
2020-02-19 08:20:25 +0700

    authentication: add routes & views

---
+++ resources/lang/en/common.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'login' => 'Login',
+    'register' => 'Register',
+    'logout' => 'Logout',
+    'remember_me' => 'Remember Me',
+    'name' => 'Name',
+    'passwd' => 'Password',
+    'forgot_passwd' => 'Forgot Your Password?',
+    'confirm_passwd' => 'Confirm Password',
+    'reset_passwd' => 'Reset Password',
+    'reset_passwd_link' => 'Send Password Reset Link',
+    'email' => 'Email Address',
+    'email_success' => 'A fresh verification link has been sent to your email address.',
+    'email_noti' => 'Before proceeding, please check your email for a verification link.',
+    'email_not_receive' => 'If you did not receive the email',
+    'email_request' => 'click here to request another',
+    'dashboard' => 'Dashboard',
+    'dashboard_msg' => 'You are logged in!',
+];
